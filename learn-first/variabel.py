aef6a1cfa3b17ac6673d7dbf3862b93ef8306703
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-11-19 22:12:30 +0700

    Learn Python | Practice

---
+++ learn-first/variabel.py
@@ -0,0 +1,15 @@
+name = "Igo Tegar"
+age = 20
+gender = "Male"
+programStudy = "Informatics"
+faculty = "Computer Science"
+country = "Indonesian"
+coding = "Python"
+
+print(name)
+print(age)
+print(gender)
+print(programStudy)
+print(faculty)
+print(country)
+print(coding)
