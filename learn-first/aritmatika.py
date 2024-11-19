d08a45700534a153bc43912c2addbbddec90a951
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-11-19 07:13:54 +0700

    Beginner Python | Practice - Update

--- learn-first/aritmatika.py
+++ learn-first/aritmatika.py
@@ -1,6 +1,8 @@
-a = 10
-b = 20
+a = int(input("Masukan Nilai Pertama : "))
+b = int(input("Masukan Input Kedua : "))
 
+print("")
+print("==== Artimatika Logika ====")
 print("Penjumlahan dari " + str(a) + " + " + str(b) + " adalah " + str(a + b))
 print("Penkurangan dari " + str(a) + " + " + str(b) + " adalah " + str(a - b))
 print("Perkalian dari " + str(a) + " + " + str(b) + " adalah " + str(a * b))
