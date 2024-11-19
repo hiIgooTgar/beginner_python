27dfc024f1d9babd9b697ed3992e98643b409469
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-11-19 22:15:37 +0700

    Learn Python | Practice

---
+++ learn-first/input.py
@@ -0,0 +1,14 @@
+nama = input("Masukan nama : ")
+umur = input("Masukan umur : ")
+jk = input("Masukan jenis kelamin : ")
+prodi = input("Masukan program studi : ")
+fakultas = input("Masukan fakultas : ")
+universitas = input("Masukan nama universitas : ")
+
+print(nama)
+print(umur)
+print(jk)
+print(prodi)
+print(fakultas)
+print(universitas)
+
