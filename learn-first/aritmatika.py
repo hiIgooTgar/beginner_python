545c474c9ed2bf4c55905d71a070e7a8e8b31564
hiIgooTgar <igotegarprambudhy128945@gmail.com>
2024-11-19 07:08:21 +0700

    Beginner Python | Practice

---
+++ learn-first/aritmatika.py
@@ -0,0 +1,8 @@
+a = 10
+b = 20
+
+print("Penjumlahan dari " + str(a) + " + " + str(b) + " adalah " + str(a + b))
+print("Penkurangan dari " + str(a) + " + " + str(b) + " adalah " + str(a - b))
+print("Perkalian dari " + str(a) + " + " + str(b) + " adalah " + str(a * b))
+print("Pembagian dari " + str(a) + " + " + str(b) + " adalah " + str(a / b))
+print("Modulus dari " + str(a) + " + " + str(b) + " adalah " + str(a % b))
